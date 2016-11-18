e862988745fa083b2a1d0d4d713e22d09593d8e9
NaohiroHamada <fj6363jg@aa.jp.fujitsu.com>
2016-11-18 17:37:22 +0900

    光センサー、気温湿度センサー、タッチセンサー追加

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ var io = require("socket.io").listen(server);
 var userHash = {};
 
 var rpio = require('rpio');
-rpio.open(7, rpio.OUTPUT, rpio.LOW);
+rpio.open(37, rpio.OUTPUT, rpio.LOW);
 
 // 2.イベントの定義
 io.sockets.on("connection", function (socket) {
@@ -52,11 +52,11 @@ io.sockets.on("connection", function (socket) {
   socket.on('event', function(data) {   // クライアントからのイベント受信
       console.log('socket id: [' + socket.id + '] data: ' + data.onoff);
       if (data.onoff == 'on') {
-        rpio.write(7, rpio.HIGH);
+        rpio.write(37, rpio.HIGH);
         io.sockets.emit('event', { onoff: 'on' });  // 全てのクライアントへ送信
       }
       if (data.onoff == 'off') {
-        rpio.write(7, rpio.LOW);
+        rpio.write(37, rpio.LOW);
         io.sockets.emit('event', { onoff: 'off' }); // 全てのクライアントへ送信
       }
     });
@@ -86,3 +86,45 @@ function pollcb(cbpin)
 }
 rpio.poll(40, pollcb);
 rpio.poll(33, pollcb);
+
+rpio.open(21, rpio.INPUT, rpio.PULL_DOWN);
+
+function pollTouch(cbpin)
+{
+    var state = rpio.read(cbpin) ? 'Yes' : 'No';
+    io.sockets.emit('touch', { value: state });
+}
+
+rpio.poll(21, pollTouch);
+
+var BH1750 = require('bh1750');
+var light = new BH1750();
+
+setInterval(read, 1000);
+
+function read() {
+  try {
+    light.readLight(function(value) {
+      io.sockets.emit('light', { lx: value });
+    });
+  } catch(e) {
+    console.log(e);
+  }
+}
+
+var sensor = require('node-dht-sensor');
+
+setInterval(dht, 5000);
+
+function dht() {
+  sensor.read(11, 4, function(err, temperature, humidity) {
+    if (!err) {
+      io.sockets.emit('dht', { temperature: temperature.toFixed(1), humidity: humidity.toFixed(1) });
+    } else {
+      console.log(err);
+    }
+  });
+}
+
+
+
